4ac765cb4f70695968e6a4dc0fededb78bcd45af
Mehdi Samsami <mehdisamsami@live.com>
2022-11-19 20:35:48 +0330

    add setup script

---
+++ setup.py
@@ -0,0 +1,31 @@
+import codecs
+from os import path
+from setuptools import setup
+
+
+setup(
+    name='yoloface',
+    version='0.0.3',
+    description='Python library for face detection and anonymization based on YOLOv7 models.',
+    keywords=['python', 'yolo', 'yolov7', 'face', 'face detection', 'anonymization', 'computer vision'],
+    author='Mehdi Samsami',
+    author_email='mehdisamsami@live.com',
+    url='https://github.com/msamsami/yoloface',
+    long_description=codecs.open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=['yoloface'],
+    classifiers=[
+        'Topic :: Computer Vision',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        'matplotlib>=3.2.2', 'numpy>=1.18.5', 'opencv-python>=4.1.1', 'Pillow>=7.1.2', 'PyYAML>=5.3.1',
+        'requests>=2.23.0', 'scipy>=1.4.1', 'torch>=1.7.0,!=1.12.0', 'torchvision>=0.8.1,!=0.13.0', 'tqdm>=4.41.0',
+        'protobuf<4.21.3', 'tensorboard>=2.4.1', 'pandas>=1.1.4', 'onnx>=1.9.0', 'ipython', 'psutil', 'thop'
+    ],
+    extras_require={}
+)
