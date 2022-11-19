51a6aa34613111b4836ca67ac720a8b942d2bdac
Mehdi Samsami <mehdisamsami@live.com>
2022-11-19 20:35:05 +0330

    add yolov7 directory to system path

--- yoloface/__init__.py
+++ yoloface/__init__.py
@@ -1,4 +1,8 @@
-from anonymize import FaceAnonymizer
-from face import YOLOv7Configs, YOLOv7Face
+import sys
+
+sys.path.append("yolov7")
+
+from .anonymize import FaceAnonymizer
+from .face import YOLOv7Configs, YOLOv7Face
 
 __all__ = ['FaceAnonymizer', 'YOLOv7Configs', 'YOLOv7Face']
